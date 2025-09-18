069225e33cc93898a74c6388f930a6fb37450efd
Matt <macavall@microsoft.com>
2025-09-18 11:13:26 -0500

    first

--- function_app.py
+++ function_app.py
@@ -13,16 +13,17 @@ configure_azure_monitor()
 app = func.FunctionApp()
 
 # Static array to store data (class-level variable)
-class DataStore:
-    static_data = bytearray()
+
 
 @app.route(route="http1", auth_level=func.AuthLevel.ANONYMOUS)
 def http1(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    static_data = bytearray()
+
     # Add 1MB of random data to static array
     ONE_MB = 1024 * 1024  # 1MB in bytes
-    DataStore.static_data.extend(os.urandom(ONE_MB))
+    static_data.extend(os.urandom(ONE_MB))
     # array_size = len(DataStore.static_data)
     logging.info(f"Added 1MB to static array. Current size: {len(DataStore.static_data)} bytes")
 
